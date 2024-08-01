27215e8d0b8134833e91fb67fd413cf787e12fb2
David <momachilles@gmail.com>
2024-08-01 16:05:23 +0200

    Rename package.

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkService",
-            targets: ["NetworkServicePackage"]),
+            targets: ["NetworkService"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkServicePackage"),
+            name: "NetworkService"),
         .testTarget(
-            name: "NetworkServicePackageTests",
-            dependencies: ["NetworkServicePackage"]),
+            name: "NetworkServiceTests",
+            dependencies: ["NetworkService"]),
     ]
 )
